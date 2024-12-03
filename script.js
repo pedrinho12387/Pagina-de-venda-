ef31a4ad457b190c6b6bb7583dd60738f46d75a4
pedrinho12387 <00001100729926sp@al.educacao.sp.gov.br>
2024-12-03 18:09:50 -0300

    .

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function searchProducts() {
     const productCards = document.querySelectorAll("Tênis Adidas Breaknet - Branco");
 
     productCards.forEach(card => {
-        const productName = card.getAttribute("data-name").toLowerCase();
+        const productName = card.getAttribute("https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSDQbXBxQqb_ZcJcHXIL_ydnayXDkadT9L-nhe1spFhZ65Z-1dEKPzA1m1VK3ZDiCxO_x3Ir1XsKtdlczrumo1q-7VfKy51Nu89n1Vq0PuenI8erAM--yMSCg&usqp=CAE").toLowerCase();
         if (productName.includes(searchQuery)) {
             card.style.display = "block";
         } else {

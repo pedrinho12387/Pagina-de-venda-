628737ac001565cb3f1a4f76043c9f9515ca2deb
pedrinho12387 <00001100729926sp@al.educacao.sp.gov.br>
2024-12-05 14:50:14 -0300

    .

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function searchProducts() {
     const productCards = document.querySelectorAll(".product-card");
 
     productCards.forEach(card => {
-        const Tênis Adidas Breaknet - Branco = card.getAttribute("Tênis Adidas Breaknet - Branco").toLowerCase();
-        if (Tênis Adidas Breaknet - Branco.includes(searchQuery)) {
+        const productName = card.getAttribute("data-name").toLowerCase();
+        if (productName.includes(searchQuery)) {
             card.style.display = "block";
         } else {
             card.style.display = "none";
